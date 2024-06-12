b315c9870a7543ff02a428955a67a4b378c81a6e
eldar <eldar.zaripoff@gmail.com>
2024-06-12 22:33:45 +0300

    problems with Logs

--- src/main/java/ru/netology/TransferMoneyAPI/TransferMoneyApiApplication.java
+++ src/main/java/ru/netology/TransferMoneyAPI/TransferMoneyApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TransferMoneyApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TransferMoneyApiApplication.class, args);
 	}
